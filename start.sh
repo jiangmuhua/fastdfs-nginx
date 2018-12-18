9b455b469aa07a6ee57ed28d22158643a6f86c6e
jiangmuhua <11956527+jiangmuhua@users.noreply.github.com>
2018-12-19 00:23:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ip=`hostname -i`
+echo "ip address = $ip"
+
+sed -i "s/139.196.213.170/${ip}/" /etc/fdfs/storage.conf
+
 /usr/bin/fdfs_trackerd /etc/fdfs/tracker.conf restart
 /usr/bin/fdfs_storaged /etc/fdfs/storage.conf restart
 /usr/sbin/nginx -g 'daemon off;'
